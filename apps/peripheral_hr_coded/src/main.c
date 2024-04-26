8eee9fe890cf7d807e4ec3f1a893f04e24eab05c
Michael Zimmermann <michael.zimmermann@grandcentrix.net>
2024-04-26 12:53:02 +0200

    peripheral_hr_coded: stop using the non-extended advertising API
    still, not crashing

--- apps/peripheral_hr_coded/src/main.c
+++ apps/peripheral_hr_coded/src/main.c
@@ -38,8 +38,6 @@ static void notify_work_handler(struct k_work *work);
 static K_WORK_DEFINE(start_advertising_worker, start_advertising_coded);
 static K_WORK_DELAYABLE_DEFINE(notify_work, notify_work_handler);
 
-static struct bt_le_ext_adv *adv;
-
 static const struct bt_data ad[] = {
 	BT_DATA_BYTES(BT_DATA_FLAGS, (BT_LE_AD_GENERAL | BT_LE_AD_NO_BREDR)),
 	BT_DATA_BYTES(BT_DATA_UUID16_ALL, BT_UUID_16_ENCODE(BT_UUID_HRS_VAL),
@@ -47,7 +45,11 @@ static const struct bt_data ad[] = {
 					  BT_UUID_16_ENCODE(BT_UUID_DIS_VAL)),
 	BT_DATA(BT_DATA_NAME_COMPLETE, DEVICE_NAME, DEVICE_NAME_LEN)
 };
-
+static const struct bt_le_adv_param advertising_params =
+BT_LE_ADV_PARAM_INIT(BT_LE_ADV_OPT_CONNECTABLE,
+		BT_GAP_ADV_FAST_INT_MIN_2,
+		BT_GAP_ADV_FAST_INT_MAX_2,
+		NULL);
 static const struct device *const uart_dev = DEVICE_DT_GET(DT_CHOSEN(app_uart));
 
 static void connected(struct bt_conn *conn, uint8_t conn_err)
@@ -91,43 +93,17 @@ BT_CONN_CB_DEFINE(conn_callbacks) = {
 	.disconnected = disconnected,
 };
 
-static int create_advertising_coded(void)
-{
-	int err;
-	struct bt_le_adv_param param =
-		BT_LE_ADV_PARAM_INIT(BT_LE_ADV_OPT_CONNECTABLE,
-				     BT_GAP_ADV_FAST_INT_MIN_2,
-				     BT_GAP_ADV_FAST_INT_MAX_2,
-				     NULL);
-
-	err = bt_le_ext_adv_create(&param, NULL, &adv);
-	if (err) {
-		printk("Failed to create advertiser set (err %d)\n", err);
-		return err;
-	}
-
-	printk("Created adv: %p\n", adv);
-
-	err = bt_le_ext_adv_set_data(adv, ad, ARRAY_SIZE(ad), NULL, 0);
-	if (err) {
-		printk("Failed to set advertising data (err %d)\n", err);
-		return err;
-	}
-
-	return 0;
-}
-
 static void start_advertising_coded(struct k_work *work)
 {
 	int err;
 
-	err = bt_le_ext_adv_start(adv, NULL);
+	err = bt_le_adv_start(&advertising_params, ad, ARRAY_SIZE(ad), NULL, 0);
 	if (err) {
 		printk("Failed to start advertising set (err %d)\n", err);
 		return;
 	}
 
-	printk("Advertiser %p set started\n", adv);
+	printk("Advertising started\n");
 }
 
 static void bas_notify(void)
@@ -204,12 +180,6 @@ int main(void)
 	}
 
 	printk("Bluetooth initialized\n");
-
-	err = create_advertising_coded();
-	if (err) {
-		printk("Advertising failed to create (err %d)\n", err);
-		return 0;
-	}
 
 	k_work_submit(&start_advertising_worker);
 	k_work_schedule(&notify_work, K_NO_WAIT);
